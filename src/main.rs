35b79543f128b4e3e699fe89df58dd5e9a19ab71
yugogamer <yugogamer@gmail.com>
2021-11-26 15:28:34 +0100

    main game loop running

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 #[macro_use] extern crate rocket;
 use rocket_okapi::{swagger_ui::*};
-use rocket::Request;
+use rocket::{Request};
 use service::game::map::Map;
-use std::sync::RwLock;
+use core::time;
+use std::{sync::{Arc, RwLock}, thread::{self}, time::SystemTime};
+
 
 mod service;
 mod controlleur;
@@ -16,15 +18,41 @@ fn not_found(req: &Request) -> String {
 
 
 struct Game {
-    pub game : RwLock<Map>
+    pub game : Arc<RwLock<Map>>
 }
 
-
+fn make_tick(lock_game : Arc<RwLock<Map>>){
+    println!("beginning game calculation");
+    let tick_time = 50;
+    let mut tick_date: SystemTime = SystemTime::now();
+    let mut tick: u128 = 0;
+    let mut action_size = 0;
+    loop {
+        let time_elapsed = tick_date.elapsed().unwrap();
+        if time_elapsed.as_millis() < tick_time
+        {
+            let sleep_time = (tick_time - time_elapsed.as_millis() as u128).try_into().unwrap();
+            std::thread::sleep(time::Duration::from_millis(sleep_time));
+            println!("tick : {} execute in : {} ms with : {} action, sleep time : {}", tick, time_elapsed.as_millis(), action_size, sleep_time);
+        }else {
+            println!("tick : {} execute in : {} ms with : {} action, sleep time : {}", tick, time_elapsed.as_millis(), action_size, 0);
+        }
+        tick+=1;
+        tick_date = SystemTime::now();
+        let current_game = &mut *lock_game.write().unwrap();
+        action_size = current_game.attack_pile.len() + current_game.move_pile.len();
+        current_game.run();
+        drop(current_game);
+    }
+}
 
 
 #[rocket::main]
 async fn main() -> Result<(), rocket::Error> {
-    let game = Game {game : RwLock::new(Map::new())};
+    let lock = RwLock::new(Map::new());
+    let duplicate = Arc::new(lock);
+    let game = Game {game : Arc::clone(&duplicate)};
+
 
     let loader = rocket::build();
     let loader = controlleur::game::load_road(loader).manage(game).register("/",catchers![not_found]);
@@ -36,6 +64,11 @@ async fn main() -> Result<(), rocket::Error> {
             ..Default::default()
         }),
     );
+
+    thread::spawn(move || {
+        make_tick(duplicate);
+    });
+
 
 
     loader.launch().await
